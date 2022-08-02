60186ca962134d7e4ffd60e2806aebfa6a82bf1a
Naimish <naimishbbhuva@gmail.com>
2022-08-02 16:45:18 +0530

    background banner done!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <div className="app">
                 <Switch>
                     <Route path="/checkout">
+                        <Header />
                         <h1>Checkout</h1>
                     </Route>
                     <Route path="/login">
@@ -15,7 +17,7 @@ function App() {
                     </Route>
                     <Route path="/">
                         <Header />
-                        <h1>Home page</h1>
+                        <Home />
                     </Route>
                 </Switch>
             </div>
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+    return (
+        <div className="home">
+            <img
+                className="home-image"
+                src="https://i0.wp.com/www.inventiva.co.in/wp-content/uploads/2021/01/AmazonPrimeVideo-genetic.jpg"
+                alt="background poster"
+            />
+        </div>
+    );
+}
+
+export default Home;
